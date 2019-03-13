387b9437f3b83b1591c45dd0de00fe8fed29ce9b
Yasas <yasass@lolctech.com>
2019-03-13 11:39:15 +0530

    do some changes

--- src/main/java/com/example/lk/MainActivity.java
+++ src/main/java/com/example/lk/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.lk;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.common.ActCommon;
+import com.example.common.Parent;
+
+public class MainActivity extends ActCommon {
+
+    private TextView tvHello;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Parent parent = new Parent();
+
+        parent.setAbc("Sri Lanka");
+
+        tvHello = findViewById(R.id.tvHello);
+
+        tvHello.setText(parent.getAbc());
     }
 }
